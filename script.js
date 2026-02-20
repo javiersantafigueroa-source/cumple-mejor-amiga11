088b7e257d674cbedb3692689f594dec0bcdd9d8
javiersantafigueroa-source <javiersantafigueroa@gmail.com>
2026-02-19 22:10:33 -0500

    Update script.js
    Signed-off-by: javiersantafigueroa-source <javiersantafigueroa@gmail.com>

--- script.js
+++ script.js
@@ -54,23 +54,23 @@
 })();
 */
 const messages = [
-    "Are you sure?",
-    "Really sure??",
-    "Are you positive?",
-    "Pookie please...",
-    "Just think about it!",
-    "If you say no, I will be really sad...",
-    "I will be very sad...",
-    "I will be very very very sad...",
-    "Ok fine, I will stop asking...",
-    "Just kidding, say yes please! ❤️"
+    "segura?",
+    "de verdad, segura??",
+    "mmm?",
+    "dale a si!...",
+    "porfaaaa!",
+    "me pondre triste...",
+    "mmmm :c...",
+    "ya me puse triste...",
+    "esta bien, dejare de molestar...",
+    "bromeo jaaaaa, di que si :b! ❤️"
 ];
 
 let messageIndex = 0;
 
 function handleNoClick() {
     const noButton = document.querySelector('.no-button');
-    const yesButton = document.querySelector('.yes-button');
+    const yesButton = document.querySelector('.si-button');
     noButton.textContent = messages[messageIndex];
     messageIndex = (messageIndex + 1) % messages.length;
     const currentSize = parseFloat(window.getComputedStyle(yesButton).fontSize);
